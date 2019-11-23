3ac01ed0a2e949acdd4bbf0bc6132bacb105a998
Cristian Urbainski <cristianurbainskips@gmail.com>
2019-11-22 23:29:44 -0300

    Adicionado @author e @since.

--- src/main/java/com/urbainski/veiculoapi/VeiculoApiApplication.java
+++ src/main/java/com/urbainski/veiculoapi/VeiculoApiApplication.java
@@ -3,6 +3,12 @@ package com.urbainski.veiculoapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author Cristian Urbainski
+ * @since 19/11/2019
+ *
+ */
 @SpringBootApplication
 public class VeiculoApiApplication {
 
