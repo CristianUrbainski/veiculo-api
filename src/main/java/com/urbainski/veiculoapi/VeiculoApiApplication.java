5fee177cacbf1d5ebcdd0a53c4f156ad9d5ea6da
Cristian Urbainski <cristianurbainskips@gmail.com>
2019-11-24 10:03:07 -0300

    Adicionado customização ao object mapper para ignorar propriedades referentes ao hibernate na serialização de um objeto para json.

--- src/main/java/com/urbainski/veiculoapi/VeiculoApiApplication.java
+++ src/main/java/com/urbainski/veiculoapi/VeiculoApiApplication.java
@@ -2,6 +2,11 @@ package com.urbainski.veiculoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * 
@@ -14,6 +19,26 @@ public class VeiculoApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VeiculoApiApplication.class, args);
+	}
+
+	@Bean
+	public ObjectMapper getMapper() {
+		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+		builder.mixIn(Object.class, IgnoreHibernatePropertiesInJackson.class);
+		return builder.build();
+	}
+
+	/**
+	 * Classe utilizada para adicionar um mixin no object mapper do sistema para que
+	 * não serialização de objetos para json as propriedades do hibernate sejam
+	 * ignoradas.
+	 * 
+	 * @author Cristian Urbainski
+	 * @since 24/11/2019
+	 *
+	 */
+	@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+	private abstract class IgnoreHibernatePropertiesInJackson {
 	}
 
 }
